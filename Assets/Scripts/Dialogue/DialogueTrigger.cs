7c24fe077cd2f96978d0104e78b91b5653e9ab7a
Zorten <zergionaruto@hotmail.com>
2022-05-22 20:53:39 -0700

    Start button can now be selected with keyboard

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
-public class DialogueTrigger : MonoBehaviour, IPointerClickHandler
+public class DialogueTrigger : MonoBehaviour//, IPointerClickHandler
 {
   
 
+    // [Header("Ink JSON Files")]
+    // [SerializeField] private TextAsset[] inkJSON;
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
 
@@ -15,14 +17,17 @@ public class DialogueTrigger : MonoBehaviour, IPointerClickHandler
     [Header("Icons")]
     [SerializeField] private GameObject StartIcon;
     [SerializeField] private GameObject ContinueIcon;
+    [SerializeField] private GameObject CurrentIcon;
 
     private bool cont;
 
     private void Awake() 
     {
-        StartIcon.SetActive(true);
+        int index = 0;
+        CurrentIcon.SetActive(true);
         ContinueIcon.SetActive(false);
         cont = false;
+        StartCoroutine(SelectFirstButton());
     }
 
     private void Update() 
@@ -30,19 +35,31 @@ public class DialogueTrigger : MonoBehaviour, IPointerClickHandler
         if (cont && !DialogueManager.GetInstance().dialogueIsPlaying) 
         {
             Debug.Log("boutta call dialogue manager");
-            if (cont) 
-            {
-                StartIcon.SetActive(false);
-                ContinueIcon.SetActive(true);
-                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
-            }
+            cont = false;
+            CurrentIcon.SetActive(false);
+            ContinueIcon.SetActive(true);
+            // DialogueManager.GetInstance().EnterDialogueMode(inkJSON[index]);
+            // index++;
+            DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
         }
     
     }
 
-    public void OnPointerClick (PointerEventData eventData)
-        {
-            Debug.Log ("clicked");
-            cont = true;
-        }
+    public void Click()
+    {
+        InputManager.GetInstance().RegisterSubmitPressed();
+        cont = true;
+    }
+
+
+
+
+        private IEnumerator SelectFirstButton() 
+    {
+        // Event System requires we clear it first, then wait
+        // for at least one frame before we set the current selected object.
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(StartIcon.gameObject);
+    }
 }
