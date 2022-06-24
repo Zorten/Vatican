b9412c8201613aa225809e798425cf4eb9bb215b
DerpyMissile <hmai015@ucr.edu>
2022-06-24 14:57:44 -0700

    can now go between rooms + pick up items

--- Assets/Scripts/Rooms.cs
+++ Assets/Scripts/Rooms.cs
@@ -48,68 +48,162 @@ public class Rooms : MonoBehaviour
     [SerializeField] private Button sFB;
     [SerializeField] private Button sB;
 
+    [SerializeField] private Button pickupButton1;
+    [SerializeField] private Button pickupButton2;
+    [SerializeField] private Text pickupButton1Text;
+    [SerializeField] private Text pickupButton2Text;
+
     public void displayItems(){
         //Debug.Log("helo dis works");
         if(whatDay.getDays() == 1){
             nBook.pickupItem();
             hWater.pickupItem();
         }
+        Debug.Log(location);
         switch(location){
-            case AllTheRooms.FirstEntrance:
+            case (AllTheRooms)0:
                 //show the button
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.FirstHallway:
+            case (AllTheRooms)1:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.FirstLivingRoom:
+            case (AllTheRooms)2:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.FirstKitchen:
+            case (AllTheRooms)3:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                if(!food.getPickupStatus()){
+                    pickupButton1.onClick.AddListener(delegate{pickupTheActualItems(pickupButton1Text, food);});
+                    pickupButton1Text.text = "Pick up Food.";
+                }
+                if(!hWater.getPickupStatus()){
+                    pickupButton2.onClick.AddListener(delegate{pickupTheActualItems(pickupButton2Text, hWater);});
+                    pickupButton2Text.text = "Pick up Empty Bottle.";
+                }
             break;
 
-            case AllTheRooms.FirstOffice:
+            case (AllTheRooms)4:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                if(!cam.getPickupStatus()){
+                    pickupButton1.onClick.AddListener(delegate{pickupTheActualItems(pickupButton1Text ,cam);});
+                    pickupButton1Text.text = "Pick up Camera.";
+                }
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.FirstCloset:
+            case (AllTheRooms)5:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.FirstBathroom:
+            case (AllTheRooms)6:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.Basement:
+            case (AllTheRooms)7:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.SecondHallway:
+            case (AllTheRooms)8:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.SecondNursery:
+            case (AllTheRooms)9:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                if(!sCrux.getPickupStatus()){
+                    pickupButton1.onClick.AddListener(delegate{pickupTheActualItems(pickupButton1Text ,sCrux);});
+                    pickupButton1Text.text = "Pick up Silver Crucifix.";
+                }
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.SecondMasterBedroom:
+            case (AllTheRooms)10:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                if(!bSheet.getPickupStatus()){
+                    pickupButton1.onClick.AddListener(delegate{pickupTheActualItems(pickupButton1Text ,bSheet);});
+                    pickupButton1Text.text = "Pick up Bedsheets.";
+                }
+                if(!wCrux.getPickupStatus()){
+                    pickupButton2.onClick.AddListener(delegate{pickupTheActualItems(pickupButton2Text ,wCrux);});
+                    pickupButton2Text.text = "Pick up Wooden Crucifix.";
+                }
             break;
 
-            case AllTheRooms.SecondFullBathroom:
+            case (AllTheRooms)11:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                pickupButton1Text.text = "";
+                pickupButton2Text.text = "";
             break;
 
-            case AllTheRooms.SecondBalcony:
+            case (AllTheRooms)12:
+                pickupButton1.onClick.RemoveAllListeners();
+                pickupButton2.onClick.RemoveAllListeners();
+                if(!therM.getPickupStatus()){
+                    pickupButton1.onClick.AddListener(delegate{pickupTheActualItems(pickupButton1Text ,therM);});
+                    pickupButton1Text.text = "Pick up Thermometer.";
+                }
+                pickupButton2Text.text = "";
             break;
         }
     }
 
+    public void pickupTheActualItems(Text theButton, Items whatItem){
+        if(whatItem.getPickupStatus()){
+            theButton.text = "";
+        }else{
+            whatItem.pickupItem();
+            theButton.text = "";
+        }
+    }
+
     public void moveRooms(){
-        everythingElse.SetActive(false);
-        roomMenu.SetActive(true);
-        displayItems();
+        if(everythingElse.activeInHierarchy){
+            everythingElse.SetActive(false);
+            roomMenu.SetActive(true);
+            displayItems();
+        }else{
+            everythingElse.SetActive(true);
+            roomMenu.SetActive(false);
+        }
     }
 
     public void travel(string whereTo){
         //location = AllTheRooms. + whereTo;
-        Debug.Log(whereTo);
+        //Debug.Log(whereTo);
         location = (AllTheRooms)System.Enum.Parse( typeof(AllTheRooms), whereTo );
         currRoomText.text = "Room: " + whereTo;
         everythingElse.SetActive(true);
         roomMenu.SetActive(false);
+        displayItems();
     }
     // Start is called before the first frame update
     void Start()
